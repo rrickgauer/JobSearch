72c57f47fd437aa63db951a827fe1b1661e23c9a
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-21 01:32:38 -0600

    update form

--- src/add-position-page.php
+++ src/add-position-page.php
@@ -3,22 +3,29 @@
 <html lang="en" dir="ltr">
 
 <head>
-	<?php include('header.php'); ?>
+  <?php include('header.php'); ?>
 
   <!-- select2 -->
   <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
 
-	<title>Add Position</title>
+  <!-- select2 bootstrap theme -->
+  <link href="css/select2-bootstrap4.css" rel="stylesheet" />
+
+  <!-- flatpickr -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+  <title>Add Position</title>
 </head>
 
 <body>
-	<?php include('navbar.php'); ?>
-	<div class="container">
-		<h1>Add position</h1>
+  <?php include('navbar.php'); ?>
+  <div class="container">
+    <h1>Add position</h1>
 
 
-		<form class="form" method="post">
+    <form class="form" method="post">
 
       <!-- company -->
       <div class="form-group">
@@ -35,27 +42,44 @@
 
 
       <!-- position -->
-			<div class="form-group">
-				<label for="position"><b>Position:</b></label>
-				<input type="text" class="form-control" id="position">
-			</div>
+      <div class="form-group">
+        <label for="position"><b>Position:</b></label>
+        <input type="text" class="form-control" id="position">
+      </div>
+
+      <!-- date -->
+      <div class="form-group">
+        <label for="date"><b>Date:</b></label>
+        <input type="date" class="form-control" id="date">
+      </div>
+
+      
 
 
 
-		</form>
-	</div>
+    </form>
+  </div>
 
   <script>
+    $(document).ready(function() {
 
-  $(document).ready(function() {
+      // set the company select inut to select2
+      $("#company").select2({
+        tags: true,
+        theme: 'bootstrap4',
+        placeholder: "Select a company",
+        allowClear: true
+      });
 
-    // set the company select inut to select2
-    $("#company").select2({
-      tags: true
+      // enable the flatpickr date plugin
+      $("#date").flatpickr({
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        defaultDate: "today",
+      });
+
     });
-
-  });
-
   </script>
 
 </body>
