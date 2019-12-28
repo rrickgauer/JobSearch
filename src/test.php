92d20ff00609cc4b5587ebfc47564d0b9f2d4375
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-28 12:09:16 -0600

    custom search box

--- src/test.php
+++ src/test.php
@@ -1,16 +1,9 @@
 <?php include('functions.php'); ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
 <head>
   <?php include('header.php'); ?>
-
- 
-
   <title>Add Position</title>
 </head>
 
@@ -21,49 +14,21 @@
 
     <h1>Test</h1>
 
-    <form class="form" method="post">
 
-      <select class="form-control" name="select-me">
-        <option value="yo">yo</option>
-        <option value="2">2</option>
-      </select> <br>
-
-      <input type="submit" name="hi" class="btn btn-primary" value="Submit">
-      
+    <div class="container custom-container">
+      <input type="text" class="custom" placeholder="search">
+      <button class="clear-btn"><i class='bx bx-x-circle'></i></button>
+    </div>
 
 
-    </form>
-
-    <?php
-
-    echo '<h1>' . $_POST['select-me'] . '</h1>';
 
 
-    ?>
 
 
 
 
   </div>
-  
 
-  <script>
-    $(document).ready(function() {
-
-      // set the company select input to select2
-      $("select").select2({
-        tags: true,
-        theme: 'bootstrap4',
-        placeholder: "Select a company",
-        allowClear: true
-      });
-
-      
-
-      
-
-    });
-  </script>
 
 </body>
 
