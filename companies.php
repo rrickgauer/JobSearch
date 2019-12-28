199c48dfd17451137d784aa5c4cab2a935ec8551
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-28 01:02:46 -0600

    add companies.php

---
+++ companies.php
@@ -0,0 +1,18 @@
+<?php include('functions.php'); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('header.php'); ?>
+    <title>Companies</title>
+  </head>
+  <body>
+    <div class="container">
+
+      <?php include('navbar.php'); ?>
+
+      <h1>Companies</h1>
+
+    </div>
+
+  </body>
+</html>
