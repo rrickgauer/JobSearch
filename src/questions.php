2b1a1355d8bb8a591c702f25ebf3f4128cba2f73
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-28 21:06:36 -0600

    add questions.php

---
+++ src/questions.php
@@ -0,0 +1,27 @@
+<?php include('functions.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('header.php'); ?>
+    <title>Questions</title>
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+
+    <div class="container">
+
+      <input type="text" name="question" placeholder="Enter question">
+
+
+
+
+
+
+
+
+
+    </div>
+
+  </body>
+</html>
