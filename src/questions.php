417622217176ec7e662651a58c0685fb3867d5a0
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-30 20:09:23 -0600

    update

--- src/questions.php
+++ src/questions.php
@@ -1,17 +1,29 @@
 <?php include('functions.php'); ?>
 
+<?php
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <?php include('header.php'); ?>
-    <title>Questions</title>
-  </head>
-  <body>
-    <?php include('navbar.php'); ?>
 
-    <div class="container">
+<head>
+  <?php include('header.php'); ?>
+  <title>Questions</title>
+</head>
 
-      <input type="text" name="question" placeholder="Enter question">
+<body>
+  <?php include('navbar.php'); ?>
+
+  <div class="container">
+
+    <h2>New Question</h2>
+    <form class="form" method="post">
+      <input type="text" name="question" class="form-control" placeholder="Question">
+      <textarea name="answer" rows="12" class="form-control" placeholder="Answer"></textarea>
+      <input type="submit" value="Submit" class="btn btn-primary">
+    </form>
 
 
 
@@ -21,7 +33,9 @@
 
 
 
-    </div>
 
-  </body>
+  </div>
+
+</body>
+
 </html>
