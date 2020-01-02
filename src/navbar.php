4c3d9cf8f7e05825a711d9a01d2612bc7ac44ed9
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-01 21:30:08 -0600

    css

--- src/navbar.php
+++ src/navbar.php
@@ -1,7 +1,7 @@
 <?php include_once('functions.php'); ?>
 <section id="top-navbar">
   <div class="container-fluid">
-    <nav class="navbar navbar-toggleable-sm navbar-expand-sm navbar-dark bg-dark">
+    <nav class="navbar navbar-toggleable-sm navbar-expand-sm">
       <!-- <div class="navbar-brand"></div> -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <ion-icon name="menu" class="custom-text-white"></ion-icon>
