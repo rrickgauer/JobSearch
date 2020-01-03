7a809b2c41bc77ad661e2b18d29b7df8bb4c0304
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-03 16:29:35 -0600

    display original question values for inputs

--- src/edit-question.php
+++ src/edit-question.php
@@ -1,4 +1,8 @@
-<?php include('functions.php'); ?>
+<?php
+include('functions.php');
+$question = getQuestion($_GET['questionID']);
+$question = $question->fetch(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -14,8 +18,8 @@
 		<h1 class="custom-font">Edit Question</h1>
 
 		<form class="form" method="post">
-			<input type="text" name="question" class="form-control" value="" placeholder="Question"> <br>
-			<textarea name="answer" rows="15" class="form-control" placeholder="Answer"></textarea><br>
+			<input type="text" name="question" class="form-control" value="<?php echo $question['question']; ?>" placeholder="Question"> <br>
+			<textarea name="answer" rows="15" class="form-control" placeholder="Answer"><?php echo $question['answer']; ?></textarea><br>
       <input type="submit" name="" value="Save" class="btn btn-primary">
 		</form>
 	</div>
